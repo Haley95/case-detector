5e2846de9de4d6c78060909669447a0b7daca9a3
haley <haleyhenson76@yahoo.com>
2020-02-25 23:04:25 -0600

    first commit

---
+++ case-detector.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+int a, A;
+char x;
+if (x == a){
+    cout<< "This is a lower case letter. \n";
+    cout<< -1 << endl;
+}
+if(x == A)
+{
+    
+    cout<< "This is an upper case letter. \n";
+    cout<< 1 << endl;
+}
+else
+{
+    cout<< "Please only input letters for this program.";
+    cout<< 0 << endl;
+}
+return 0;
+}
